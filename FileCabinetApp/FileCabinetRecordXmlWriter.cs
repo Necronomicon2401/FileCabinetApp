2bc0b074e120b2c598ebc306916b4453df597483
Necronomicon2401 <bolbadim@gmail.com>
2021-09-17 18:19:03 +0300

    Add export of file cabinet records to CSV file. Add export of file cabinet records to XML file.

---
+++ FileCabinetApp/FileCabinetRecordXmlWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// XML writer class.
+    /// </summary>
+    public class FileCabinetRecordXmlWriter
+    {
+        private readonly XmlWriter writer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileCabinetRecordXmlWriter"/> class.
+        /// </summary>
+        /// <param name="writer">Writer to use.</param>
+        public FileCabinetRecordXmlWriter(XmlWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        /// <summary>
+        /// Write the records by using current writer.
+        /// </summary>
+        /// <param name="records">Records for writing.</param>
+        public void Write(FileCabinetRecord[] records)
+        {
+            XmlSerializer serializer = new (records.GetType());
+            serializer.Serialize(this.writer, records);
+        }
+    }
+}
---
+++ FileCabinetApp/FileCabinetServiceSnapshot.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// Class for FileCabinetService snapshots.
+    /// </summary>
+    public class FileCabinetServiceSnapshot
+    {
+        private readonly FileCabinetRecord[] records;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileCabinetServiceSnapshot"/> class.
+        /// </summary>
+        /// <param name="list">List of records.</param>
+        public FileCabinetServiceSnapshot(List<FileCabinetRecord> list)
+        {
+            this.records = list.ToArray();
+        }
+
+        /// <summary>
+        /// Creates CSV record.
+        /// </summary>
+        /// <param name="streamWriter">Stream writer to use.</param>
+        public void SaveToCsv(StreamWriter streamWriter)
+        {
+            FileCabinetRecordCsvWriter fileCabinetRecordCsvWriter = new (streamWriter);
+            streamWriter.WriteLine("Id,First Name,Last Name,Date of Birth,Work Experience,Weight,Lucky symbol");
+            foreach (var record in this.records)
+            {
+                fileCabinetRecordCsvWriter.Write(record);
+            }
+        }
+
+        /// <summary>
+        /// Creates XML record.
+        /// </summary>
+        /// <param name="streamWriter">Stream writer to use.</param>
+        public void SaveToXml(StreamWriter streamWriter)
+        {
+            XmlWriterSettings settings = new ();
+            settings.ConformanceLevel = ConformanceLevel.Auto;
+            settings.Indent = true;
+            settings.NewLineOnAttributes = true;
+            XmlWriter writer = XmlWriter.Create(streamWriter, settings);
+            FileCabinetRecordXmlWriter fileCabinetRecordXmlWriter = new (writer);
+            fileCabinetRecordXmlWriter.Write(this.records);
+            writer.Close();
+        }
+    }
+}
